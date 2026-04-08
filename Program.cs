67e02ac1ef4c53cd7fe323aa1ab0d7ed73cbdcda
unknown <omar.bunisher.ri2020@gmail.com>
2026-04-08 19:47:07 +0300

    feat: add URL validation on POST /shorten

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -7,6 +9,17 @@ app.MapGet("/", () => "Hello World!");
 app.MapPost("/shorten", (ShortenRequest request) =>
 {
 
+    if (string.IsNullOrWhiteSpace(request.LongUrl))
+    {
+        return Results.BadRequest(new { error = "URL cannot be empty" });
+    }
+    bool isValidUri = Uri.TryCreate(request.LongUrl, UriKind.Absolute, out Uri? uri);
+    bool isWebUri = isValidUri && (uri?.Scheme == Uri.UriSchemeHttp || uri?.Scheme == Uri.UriSchemeHttps);
+
+    if (!isWebUri)
+    {
+        return Results.BadRequest(new { error = "The URL is invalid. Please provide a valid HTTP or HTTPS link." });
+    }
     string code = urlStore.Add(request.LongUrl);
     return Results.Ok(new { shortCode = code });
 });
