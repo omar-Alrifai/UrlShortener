e72c6014a4bce71e2228ac218d847870584d5a09
unknown <omar.bunisher.ri2020@gmail.com>
2026-04-16 00:42:03 +0300

    refactor : v0.4 update Program.cs to inject IUrlShortenerService

--- Program.cs
+++ Program.cs
@@ -1,41 +1,40 @@
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddProblemDetails();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<UrlStore>();
+builder.Services.AddScoped<IShortLinkRepository, EfShortLinkRepository>();
+builder.Services.AddScoped<IUrlShortenerService, UrlShortenerService>();
 var app = builder.Build();
 
 app.UseStatusCodePages();
 app.UseExceptionHandler();
 
-// UrlStore urlStore = new();
+
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/shorten", async (ShortenRequest request, UrlStore urlStore) =>
+app.MapPost("/shorten", async (ShortenRequest request, IUrlShortenerService urlShortenerService) =>
 {
-
-
-    var (isValid, ErrorMessage) = UrlValidator.Validate(request.LongUrl);
-    if (!isValid)
+    try
+    {
+        string code = await urlShortenerService.ShortenUrlAsync(request.LongUrl!);
+        return Results.Ok(new { shortCode = code });
+    }
+    catch (ArgumentException ex)
     {
         return Results.Problem(
-            detail: ErrorMessage,
-            statusCode: StatusCodes.Status400BadRequest,
-            title: "Validation Error"
-        );
+           detail: ex.Message,
+           statusCode: StatusCodes.Status400BadRequest,
+           title: "Validation Error"
+       );
     }
-
-    string code = await urlStore.Add(request.LongUrl!);
-    return Results.Ok(new { shortCode = code });
 });
 
 
-app.MapGet("/{code}", async (string code, UrlStore urlStore) =>
+app.MapGet("/{code}", async (string code, IUrlShortenerService urlShortenerService) =>
 {
-    string? url = await urlStore.TryGet(code);
-    return url != null ? Results.Redirect(url!) : Results.Problem(
+    string? url = await urlShortenerService.GetUrlAsync(code);
+    return url != null ? Results.Redirect(url) : Results.Problem(
         detail: $"The short code '{code}' was not found.",
         statusCode: StatusCodes.Status404NotFound,
         title: "Short code not found"
@@ -44,3 +43,5 @@ app.MapGet("/{code}", async (string code, UrlStore urlStore) =>
 app.Run();
 
 record ShortenRequest(string? LongUrl);
+
+
