c3efbbf3f99455c429c5755d4b1e62337659c8f9
unknown <omar.bunisher.ri2020@gmail.com>
2026-04-27 23:21:01 +0300

    refactor : v0.6 add method IsValidCustomCode to class ValidatorHelper which was previously named UrlValidator

--- UrlShortener/ValidatorHelper.cs
+++ UrlShortener/ValidatorHelper.cs
@@ -1,4 +1,6 @@
-public static class UrlValidator
+using System.Text.RegularExpressions;
+
+public static class ValidatorHelper
 {
     public static (bool isValid, string? ErrorMessage) Validate(string? url)
     {
@@ -13,5 +15,10 @@ public static class UrlValidator
             return (false, "The URL is invalid. Please provide a valid HTTP or HTTPS link.");
         }
         return (true, null);
+    }
+    public static bool IsValidCustomCode(string code)
+    {
+        return code.Length >= 6 && code.Length <= 10 &&
+               Regex.IsMatch(code, "^[a-zA-Z0-9]+$");
     }
 }
