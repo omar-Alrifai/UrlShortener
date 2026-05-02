e2d1a960e39346342de0b0dbf07d8262d7f124c5
unknown <omar.bunisher.ri2020@gmail.com>
2026-05-02 18:02:27 +0300

    feat(ui) : v0.6 send ExpiresAt to API and store full link data

--- url-shortener-ui/url-shortener-ui/src/composables/useUrlShortener.js
+++ url-shortener-ui/url-shortener-ui/src/composables/useUrlShortener.js
@@ -6,35 +6,57 @@ export function useUrlShortener() {
   const error = ref("");
   const loading = ref(false);
   const history = ref([]);
+  const currentLink = ref(null);
+
 
   const apiBase = "http://localhost:5021";
 
-  const shortenUrl = async (url, customAlias = null) => {
+  const toUtcIso = (localDateTime) => {
+    if (!localDateTime) return null;
+    const date = new Date(localDateTime);
+    console.log(date.getTime());
+    console.log(isNaN(date.getTime()));
+    if (isNaN(date.getTime())) return null;
+    return date.toISOString();
+  };
+
+
+  const shortenUrl = async (url, customAlias = null, localExpiry = null) => {
     error.value = "";
     shortUrl.value = "";
     loading.value = true;
+        currentLink.value = null;
+
 
     try {
       const payload = { longUrl: url };
       if (customAlias && customAlias.trim() !== "") {
         payload.customCode = customAlias.trim();
       }
-      
+        const expiryUtc = toUtcIso(localExpiry);
+      if (expiryUtc) {
+        payload.ExpiresAt = expiryUtc;
+      }
 
       const response = await axios.post(`${apiBase}/shorten`, payload);
       const link = response.data.shortLink;
 
       const generatedLink = `${apiBase}/${link.code}`;
       shortUrl.value = generatedLink;
+      currentLink.value = { ...link, shortUrl: generatedLink };
 
       history.value.unshift({
         longUrl: link.longUrl,
         shortUrl: generatedLink,
         clicks: link.clicks,
+        expiresAt: link.expiresAt,
+        code: link.code,
       });
-    } catch (err) {
+    }  catch (err) {
       if (err.response?.status === 409) {
         error.value = err.response?.data?.detail || "Custom code already taken. Please choose another.";
+      } else if (err.response?.status === 410) {
+        error.value = err.response?.data?.detail || "This link has expired.";
       } else {
         error.value = err.response?.data?.detail || "An unexpected error occurred.";
       }
@@ -49,5 +71,6 @@ export function useUrlShortener() {
     loading,
     history,
     shortenUrl,
+    currentLink
   };
 }
